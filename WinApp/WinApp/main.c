32973109db209b2ebbb5a8d8ac174549011c93f6
fatihmetin fatihmetin <fatihmetin@mail.com>
2018-08-15 23:33:50 +0300

    Sample of creating childWindow in switch case loop..

--- WinApp/WinApp/main.c
+++ WinApp/WinApp/main.c
@@ -1,15 +1,14 @@
 #include <windows.h>
-
-
 #define ID_CHILD	100
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam,LPARAM lParam);
 LRESULT CALLBACK WndChildProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 	LPSTR lpszCmdParam, int nCmdShow)
 {
 	WNDCLASS wndClass;
-	HWND hWnd, hWndChild;
+	HWND hWnd;
 	MSG message;
 	if (!hPrevInstance) {
 		wndClass.style = CS_HREDRAW | CS_VREDRAW;
@@ -39,6 +38,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 		if (!RegisterClass(&wndClass))
 			return -1;
 	}
+
 	hWnd = CreateWindow("Generic", "Generic App",
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT,
@@ -49,15 +49,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 		NULL,
 		hInstance,
 		NULL);
-	    if (!hWnd)
-			return -1;
-	hWndChild = CreateWindow("SampleChild", "Generic App",
-		WS_CHILD | WS_VISIBLE | WS_BORDER | WS_CAPTION | WS_THICKFRAME,
-		100, 100, 100, 100, hWnd,
-		(HMENU)ID_CHILD,
-		hInstance,
-		NULL);
-	if (!hWndChild)
+	if (!hWnd)
 		return -1;
 	ShowWindow(hWnd, SW_RESTORE);
 	UpdateWindow(hWnd);
@@ -70,9 +62,22 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam,
 	LPARAM lParam)
 {
+	static HWND s_hWndChild;
 	switch (message) {
+	case WM_CREATE:
+		s_hWndChild = CreateWindow("SampleChild", "Generic App",
+			WS_CHILD | WS_VISIBLE | WS_BORDER | WS_CAPTION | WS_THICKFRAME,
+			100, 100, 100, 100, hWnd,
+			(HMENU)ID_CHILD,
+			((LPCREATESTRUCT)lParam)->hInstance, NULL);
+		if (!s_hWndChild)
+			return -1;
+		break;
 	case WM_LBUTTONDOWN:
-		MessageBox(hWnd, "Parent window", "Message", MB_OK);
+		SendMessage(s_hWndChild, WM_USER, 0, 0);
+		break;
+	case WM_USER:
+		MessageBox(hWnd, "Message From Child!..", "Message", MB_OK);
 		break;
 	case WM_DESTROY:
 		PostQuitMessage(0);
@@ -86,11 +91,11 @@ LRESULT CALLBACK WndChildProc(HWND hWnd, UINT message, WPARAM wParam,
 	LPARAM lParam)
 {
 	switch (message) {
-	case WM_LBUTTONDOWN:
-		MessageBox(hWnd, "Child window", "Message", MB_OK);
+	case WM_USER:
+		MessageBox(hWnd, "Message From Parent!..", "Message", MB_OK);
 		break;
-	case WM_DESTROY:
-		PostQuitMessage(0);
+	case WM_LBUTTONDOWN:
+		SendMessage(GetParent(hWnd), WM_USER, 0, 0);
 		break;
 	default:
 		return DefWindowProc(hWnd, message, wParam, lParam);
